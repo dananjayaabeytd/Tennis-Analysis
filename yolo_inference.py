9eeddcd25d99bc2299ba104b9b176c79004f8c5a
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2024-12-24 14:47:04 +0530

    train ball detector and court keypoints detector

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,15 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8x")
+# model = YOLO("yolov8x")
+model = YOLO("models/yolo5_last.pt")
 
-model.predict('input_videos/image.png',save=True)
-# result = model.track('input_videos/image.png',save=True)
+# model.predict('input_videos/image.png',save=True)
+result = model.predict('input_videos/input_video.mp4',conf=0.2,save=True)
+# result = model.predict('input_videos/image.png',save=True)
+
+
+
+print(result)
+print("boxes:")
+for box in result[0].boxes:
+    print(box)
