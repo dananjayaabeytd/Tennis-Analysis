f9b6188e48e3acd7764fbf1a4138d3a9d99919bd
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2024-12-24 16:46:04 +0530

    player tracker

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+import os
+from trackers import PlayerTracker
+from utils import (
+    read_video,
+    save_video
+    )
+
+
+# Set environment variable to avoid OpenMP error
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 def main():
-    print("Hello World")
     
+   # Read Video
+    input_video_path = "input_videos/input_video.mp4"
+    video_frames = read_video(input_video_path)
+    
+    player_tracker = PlayerTracker(model_path="yolov8x")
+    
+    player_detections = player_tracker.detect_frames(video_frames,read_from_stub=False,stub_path="tracker_stubs/player_detections.pkl")
+                  
+                  
+    # Draw Output
+    # Draw Player bounding boxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)                                                
+    
+    save_video(video_frames, "output_videos/output_video.avi")
     
 if __name__ == "__main__":
     main()
