72d37f0d8333d0694638e1cc959c92928b6ed75b
Tharindu Dananjaya Abenayaka <155418943+dananjayaabeytd@users.noreply.github.com>
2024-12-26 13:52:03 +0530

    ball tracker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
+
 from utils import (
     read_video,
     save_video
@@ -15,14 +16,28 @@ def main():
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
     
-    player_tracker = PlayerTracker(model_path="yolov8x")
     
-    player_detections = player_tracker.detect_frames(video_frames,read_from_stub=False,stub_path="tracker_stubs/player_detections.pkl")
+    # Detect players and ball
+    player_tracker = PlayerTracker(model_path="yolov8x")
+    ball_tracker = BallTracker(model_path="models/yolo5_last.pt")
+    
+    player_detections = player_tracker.detect_frames(
+                                                    video_frames,
+                                                    read_from_stub=True,
+                                                    stub_path="tracker_stubs/player_detections.pkl"
+                                                    )
+    
+    ball_detections = ball_tracker.detect_frames(
+                                                video_frames,
+                                                read_from_stub=True,
+                                                stub_path="tracker_stubs/ball_detections.pkl"
+                                                )
                   
                   
     # Draw Output
     # Draw Player bounding boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)                                                
+    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)                                                
     
     save_video(video_frames, "output_videos/output_video.avi")
     
